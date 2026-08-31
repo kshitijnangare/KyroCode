7af7e8390b4d91551ae0346e9a7f4f7f0abda87c
kshitijnangare <kshitijnangarework@gmail.com>
2026-08-31 19:31:43 +0530

    Created RatingHistory Table

---
+++ backend/models/RatingHistory.js
@@ -0,0 +1,61 @@
+import { sequelize } from "../config/database";
+import { DataTypes } from "sequelize";
+
+const RatingHistory = sequelize.define(
+    "RatingHistory",
+    {
+        rating_history_id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4
+        },
+        user_id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: "users",
+                key: "user_id"
+            }
+        },
+        contest_id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: "contests",
+                key: "contest_id"
+            }
+        },
+        old_rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        new_rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        rank: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+        },
+        recorded_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        }
+    },
+    {
+        tableName: "",
+        timestamps: true,
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+    }
+);
+
+export default RatingHistory;
