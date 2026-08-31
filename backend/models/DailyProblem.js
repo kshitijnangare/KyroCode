78b39842a181bf516597184a071b2ded1e35842c
kshitijnangare <kshitijnangarework@gmail.com>
2026-08-31 19:09:02 +0530

    Created DailyProblem Table

---
+++ backend/models/DailyProblem.js
@@ -0,0 +1,32 @@
+import { sequelize } from "../config/database";
+import { DataTypes } from "sequelize";
+
+const DailyProblem = sequelize.define(
+    "DailyProblem",
+    {
+        daily_problem_id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        problem_id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: "problems",
+                key: "problem_id"
+            }
+        }
+    },
+    {
+        tableName: "daily_problems",
+        timestamps: true,
+    }
+);
+
+export default DailyProblem;
