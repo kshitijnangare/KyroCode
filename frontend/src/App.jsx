058740a271bddbd073762174b351a2634d11585b
kshitijnangare <kshitijnangarework@gmail.com>
2026-09-05 20:14:27 +0530

    feature/frontend: basic folder setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <BrowserRouter>
+      <></>
+    </BrowserRouter>
   )
 }
 
